a609c7b42a1393d2501a9e2adf22ca77e233e8ab
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-10-24 14:57:33 -0300

    Create Data types.py

---
+++ tutorial/Data types.py
@@ -0,0 +1,34 @@
+"""Setting the data type"""
+
+x = "Hello World"   #str
+x = 20   #int
+x = 25.5 #float
+x = 1j   #complex
+x = ["apple","banana","cherry"] #list
+x = ("apple","banana","cherry") #tuple
+x = range(6)   #range
+x = {"name":"John","age": 36}   #dict
+x = {"apple","banana","charry"} #set
+x = frozenset({"apple","banana","cherry"}) #frozenset
+x = True  #bool
+x = b"Hello" #bytes
+x = bytearray(5)  #bytearray
+x = memoryview(bytes(5)) #memoryview
+x = None  #NoneType
+
+"""Setting the Specific Data Type"""
+
+x = str("Hello World")
+x = int(20)
+x = float(20.5)
+x = complex(1j)
+x = list(("apple","banana","cherry"))
+x = tuple(("apple","banana","cherry"))
+x = range(6)
+x = dict(name="John",age=36)
+x = set(("apple", "banana", "cherry"))
+x = frozenset(("apple", "banana", "cherry"))
+x = bool(5)
+x = bytes(5)
+x = bytearray(5)
+x = memoryview(bytes(5))
