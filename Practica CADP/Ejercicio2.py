6c1da490f953c4d43e128d35be242de0dccc4fac
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-10-30 10:34:26 -0300

    create :dragon:

---
+++ Practica CADP/Ejercicio2.py
@@ -0,0 +1,19 @@
+'''
+
+Modifique el programa anterior para que el mensaje de salida muestre la suma de ambos numeros:
+a)Utilizando una variable adicional
+b)Sin utilizar una variable adicional
+
+'''
+import random
+
+num1 = int(random.randrange(50))
+num2 = int(random.randrange(50))
+
+suma = num1 + num2 
+
+print("La suma de ambos numeros")
+print(suma)
+
+print("La suma de ambos numeros")
+print(num1+num2)
