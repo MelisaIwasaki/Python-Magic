9f83bef3463b296cf1cc6660b643256074087213
メリー緑 <92184167+MelisaIwasaki@users.noreply.github.com>
2022-10-30 10:32:26 -0300

    create :owl:

---
+++ Practica CADP/Ejercicio1.py
@@ -0,0 +1,14 @@
+'''
+
+Implemente un programa que lea por teclado dos numeros enteros e imprima en pantalla los valores leidos 
+en orden inverso. Por ej,si se ingresaron los numeros 4 y 8,debe mostrar el mensaje:Se ingresaron los 
+valores 8 y 4
+
+'''
+import random
+
+num1 = int(random.randrange(10))
+num2 = int(random.randrange(10))
+
+print("Se ingresaron los valores : ")
+print(num2 , num1)
